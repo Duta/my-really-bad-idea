de7a1c2bcf1f9133c0d866bd7ebb67f2a8e39449
Duta <wheen.b@gmail.com>
2013-12-03 00:05:28 +0000

    Started adding some useful methods

--- src/emu6502.c
+++ src/emu6502.c
@@ -3,14 +3,27 @@
 
 void * emu6502_new() {
   emu6502 *emu = malloc(sizeof(emu6502));
+
+  emu->ram = calloc(0x10000, sizeof(uint8_t));
+
   emu->destroy = emu6502_destroy;
-  emu->carry = emu6502_carry;
-  emu->zero = emu6502_zero;
+
+  emu->carry  = emu6502_carry;
+  emu->zero   = emu6502_zero;
   emu->intdis = emu6502_intdis;
   emu->decmod = emu6502_decmod;
-  emu->brk = emu6502_brk;
-  emu->over = emu6502_over;
-  emu->neg = emu6502_neg;
+  emu->brk    = emu6502_brk;
+  emu->over   = emu6502_over;
+  emu->neg    = emu6502_neg;
+
+  emu->getbyte      = emu6502_getbyte;
+  emu->getword      = emu6502_getword;
+  emu->immbyte      = emu6502_immbyte;
+  emu->zeropgaddr   = emu6502_zeropgaddr;
+  emu->zeropgxaddr  = emu6502_zeropgxaddr;
+  emu->zeropgyaddr  = emu6502_zeropgyaddr;
+  emu->indxaddr     = emu6502_indxaddr;
+
   return emu;
 } 
 
@@ -55,4 +68,39 @@ bool emu6502_neg(void *self) {
   emu6502 *emu = self;
   return emu->psw & (1 << 7) ? 1 : 0;
 } 
+
+uint8_t emu6502_getbyte(void *self, uint16_t addr) {
+  emu6502 *emu = self;
+  return emu->ram[addr];
+}
+
+uint16_t emu6502_getword(void *self, uint16_t addr) {
+  emu6502 *emu = self;
+  return emu->ram[addr] + (emu->ram[addr+1] << 8);
+}
+
+uint8_t emu6502_immbyte(void *self) {
+  emu6502 *emu = self;
+  return emu->ram[emu->pc];
+}
+
+uint8_t emu6502_zeropgaddr(void *self) {
+  emu6502 *emu = self;
+  return emu->ram[emu->pc];
+}
+
+uint8_t emu6502_zeropgxaddr(void *self) {
+  emu6502 *emu = self;
+  return emu->x + emu->ram[emu->pc];
+}
+
+uint8_t emu6502_zeropgyaddr(void *self) {
+  emu6502 *emu = self;
+  return emu->y + emu->ram[emu->pc];
+}
+
+uint16_t emu6502_indxaddr(void *self) {
+  emu6502 *emu = self;
+  return emu->getword(emu, emu->x + emu->ram[emu->pc]);
+}
 
--- src/emu6502.h
+++ src/emu6502.h
@@ -5,6 +5,8 @@
 #include <stdint.h>
 
 typedef struct {
+  /* Memory */
+  uint8_t *ram;
   /* Program Counter */
   uint16_t pc;
   /* Index Registers */
@@ -15,7 +17,7 @@ typedef struct {
   uint8_t psw;
   /* Stack Pointer */
   uint8_t sp;
-  /* Has flags testing */
+  /* Flags Testing */
   bool (*carry)(void *self);  /* Carry Flag */
   bool (*zero)(void *self);   /* Zero Flag */
   bool (*intdis)(void *self); /* Interrupt Disable */
@@ -23,12 +25,21 @@ typedef struct {
   bool (*brk)(void *self);    /* Break Command */
   bool (*over)(void *self);   /* Overflow Flag */
   bool (*neg)(void *self);    /* Negative Flag */
+  /* Helpful Methods */
+  uint8_t (*getbyte)(void *self, uint16_t addr);
+  uint16_t (*getword)(void *self, uint16_t addr);
+  uint8_t (*immbyte)(void *self);
+  uint8_t (*zeropgaddr)(void *self);
+  uint8_t (*zeropgxaddr)(void *self);
+  uint8_t (*zeropgyaddr)(void *self);
+  uint16_t (*indxaddr)(void *self);
   /* Destructor */
   void (*destroy)(void *self);
 } emu6502;
 
 void * emu6502_new();
 void emu6502_destroy(void *self);
+
 bool emu6502_carry(void *self);
 bool emu6502_zero(void *self);
 bool emu6502_intdis(void *self);
@@ -36,5 +47,13 @@ bool emu6502_decmod(void *self);
 bool emu6502_brk(void *self);
 bool emu6502_over(void *self);
 bool emu6502_neg(void *self);
+
+uint8_t emu6502_getbyte(void *self, uint16_t addr);
+uint16_t emu6502_getword(void *self, uint16_t addr);
+uint8_t emu6502_immbyte(void *self);
+uint8_t emu6502_zeropgaddr(void *self);
+uint8_t emu6502_zeropgxaddr(void *self);
+uint8_t emu6502_zeropgyaddr(void *self);
+uint16_t emu6502_indxaddr(void *self);
 
 #endif /* EMU6502_H */
